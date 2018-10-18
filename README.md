cede79ec36462428fd8932a50a09edd578719b81
bearchen <cxdtotsj@163.com>
2018-10-18 17:49:40 +0800

    1.自己写后台模板

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 练习django开发
+
+
+# 说明
+
+1. 替换 PIP 安装的源
+
+    清华源(推荐，5分钟同步一次) : `https://pypi.tuna.tsinghua.edu.cn/simple`
+    豆瓣源 : `http://pypi.douban.com/simple/`
+
+    命令 : `pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests`
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from sign.models import Event
 
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+# 登陆操作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user) # 登陆
+            request.session['user'] = username # 将 session 记录到浏览器
+            response = HttpResponseRedirect('/event_manage/')
+            # response.set_cookie('user',username,3600) # 添加浏览器 cookie
+            return response
+            
+        else:
+            return render(request,'index.html',{'error':'username or password error'})
+    else:
+        return HttpResponse("Method Not Allowed")
+
+# 发布会管理
+@login_required
+def event_manage(request):
+    # username = request.COOKIES.get('user','') # 读取浏览器 cookie
+
+    event_list = Event.objects.all()
+    username = request.session.get('user','') # 读取浏览器 session
+    return render(request,"event_manage.html",{"user":username,"events":event_list})
